513d1cbf800c050bdd2b66a3ef3871a814616dbc
Chris Mochinski <cmochinski@gmail.com>
2024-09-14 18:02:46 -0500

    coming soon dialogue to avoid confusion

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -31,7 +31,7 @@ export function Home() {
       const randomCloud = cloudImages[Math.floor(Math.random() * cloudImages.length)];
       const randomY = Math.random() * 40; // Random Y position in percentage
       const randomSize = Math.random() * (700 - 200) + 200; // Random size between 200px and 700px
-      const randomSpeed = Math.random() * (80 - 30) + 30; // Random speed between 30s and 80s
+      const randomSpeed = Math.random() * (90 - 20) + 20; // Random speed between 30s and 90s
       const randomOpacity = Math.random() * (0.6 - 0.1) + 0.1; // Random opacity between 0.1 and 0.6
       const randomZIndex = Math.floor(Math.random() * 2) - 1; // Random z-index between -1 and 1
 
@@ -81,6 +81,9 @@ export function Home() {
           <div className="homeContentWrapper">
             <div id="harvestmen-button" role="button">
               <h1>Harvestmen</h1>
+            </div>
+            <div className="comingSoonText">
+                <h2>Coming Soon</h2>
             </div>
           </div>
           <div className="imageWrapper">
