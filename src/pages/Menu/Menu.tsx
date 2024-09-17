0ade2bb7c4adc6885c60185a0b8b7a1afb21dde9
Chris Mochinski <cmochinski@gmail.com>
2024-09-17 00:12:27 -0500

    subtle bg animation island

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import "./Menu.scss";
 import ferris from "../../pages/Home/ferris-1.png";
 
 export function Menu() {
+  const [isHovered, setIsHovered] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuOpen = () => {
+    console.log("menu open clicked!");
+    setMenuOpen(true);
+  };
+
   return (
     <div className="menu">
-      <img className="ferrisWheel" src={ferris} alt="Floating Ferris Wheel Logo" />
+      <img className={`ferrisWheel ${isHovered ? "hovered" : ""}`}
+       src={ferris} alt="Floating Ferris Wheel Logo" />
       <div className="titleMenuWrapper">
-        <div id="harvestmen-button" role="button">
+        <div
+          id="harvestmen-button"
+          role="button"
+          tabIndex={0}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          onClick={handleMenuOpen}>
           <h1 className="crimson-text">Harvestmen</h1>
         </div>
       </div>
